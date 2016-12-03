21e990690f1c6a1e9fc3596649ec879c2a3edfe5
ice1000 <ice1000kotlin@foxmail.com>
2016-12-03 20:53:23 +0800

    add standalone 32-bit Dev Cpp configuration file

---
+++ jni/cpp-test/pair_test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "../global/templates.hpp"
+
+using std::cout;
+using std::cin;
+using std::endl;
+using ice1000_util::Ice1000Pair;
+
+auto main(int argc, const char *argv[]) -> int {
+	auto pair = *new Ice1000Pair<int, int>(233, 666);
+//	cin >> pair;
+	cout << pair << endl;
+	return 0;
+}
+
