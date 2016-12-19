5dadb39269f529883a19a835ff38758a7adea05f
ice1000 <ice1000kotlin@foxmail.com>
2016-12-19 13:27:45 +0800

    add Constants.java

---
+++ src/org/algo4j/math/Constants.java
@@ -0,0 +1,26 @@
+package org.algo4j.math;
+
+/**
+ * Created by ice1000 on 2016/12/18.
+ *
+ * @author ice1000
+ */
+public final class Constants {
+	private Constants() {
+	}
+
+	/**
+	 * LIGHT_SPEED m/s
+	 */
+	public static final int LIGHT_SPEED = 299792458;
+
+	/**
+	 * PLANK_CONSTANT * 10^-34 Js
+	 */
+	public static final double PLANK_CONSTANT = 6.6261;
+
+	/**
+	 * ELEMENTARY_CHARGE * 10^-19 C
+	 */
+	public static final double ELEMENTARY_CHARGE = 1.6022;
+}
---
+++ test/org/algo4j/math/ConstantsTest.kt
@@ -0,0 +1,17 @@
+package org.algo4j.math
+
+import org.algo4j.math.Constants.PLANK_CONSTANT
+import org.algo4j.test.println
+import org.junit.Test
+
+/**
+ * Created by ice1000 on 2016/12/18.
+
+ * @author ice1000
+ */
+class ConstantsTest {
+	@Test
+	fun outputConstants() {
+		PLANK_CONSTANT.println()
+	}
+}
