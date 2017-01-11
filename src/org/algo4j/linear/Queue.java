47c38fa6ac4c592bfa1204d744f817f409f08b9b
ice1000 <ice1000kotlin@foxmail.com>
2017-01-11 09:05:20 +0800

    update

---
+++ src/org/algo4j/linear/Queue.java
@@ -0,0 +1,128 @@
+package org.algo4j.linear;
+
+import org.algo4j.error.QueueException;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Iterator;
+import java.util.function.Consumer;
+
+/**
+ * Created by ice1000 on 2017/1/11.
+ *
+ * @author ice1000
+ */
+@SuppressWarnings("ALL")
+public final class Queue<T> implements
+		Container<T> {
+
+	private final Object[] data;
+	private final int maxLen;
+	private int begin;
+	private int end;
+
+	@Contract(pure = true)
+	public Queue() {
+		this(1000);
+	}
+
+	@Contract(pure = true)
+	public Queue(int maxLen) {
+		this.maxLen = maxLen;
+		data = new Object[maxLen];
+		begin = 0;
+		end = 0;
+	}
+
+	private Queue(@NotNull T[] data, int begin, int end) {
+		this.data = data;
+		this.maxLen = data.length;
+		this.begin = begin;
+		this.end = end;
+	}
+
+	@Override
+	@Contract(pure = true)
+	public int size() {
+		return end - begin;
+	}
+
+	@Override
+	@Contract(pure = true)
+	public boolean empty() {
+		return size() <= 0;
+	}
+
+	/**
+	 * push an object
+	 *
+	 * @param obj object
+	 * @throws QueueException if queue overflow
+	 */
+	@Override
+	public synchronized void push(@NotNull T obj) {
+		if (size() >= maxLen) {
+			throw QueueException.overflow();
+		} else {
+			data[end++ % maxLen] = obj;
+		}
+	}
+
+	@Override
+	@NotNull
+	@Contract(value = " -> !null", pure = true)
+	public Container<T> clone() {
+		return new Queue<>((T[]) data, begin, end);
+	}
+
+	@Override
+	@Nullable
+	public synchronized T pop() {
+		return empty() ? null : (T) data[begin++ % maxLen];
+	}
+
+	@Override
+	@Nullable
+	@Contract(pure = true)
+	public T front() {
+		return empty() ? null : (T) data[begin % maxLen];
+	}
+
+	@Override
+	@NotNull
+	@Contract(value = " -> !null", pure = true)
+	public Iterator<T> iterator() {
+		return new QueueItr(this);
+	}
+
+	@Override
+	public void forEach(@NotNull Consumer<? super T> action) {
+		for (int cursor = begin; cursor <= end; ++cursor) {
+			action.accept((T) data[cursor % maxLen]);
+		}
+	}
+
+	public class QueueItr<E> implements Iterator<E> {
+		private int cursor;
+		private Queue<E> context;
+
+		@Contract(pure = true)
+		public QueueItr(@NotNull Queue<E> queue) {
+			cursor = queue.begin;
+			context = queue;
+		}
+
+		@Override
+		@Contract(pure = true)
+		public boolean hasNext() {
+			return cursor < context.end;
+		}
+
+		@Nullable
+		@Override
+		public synchronized E next() {
+			return hasNext() ? (E) context.data[cursor++ % context.maxLen] : null;
+		}
+	}
+}
