88bf81caab4f6eae3728f8442417a9fa1560250d
ice1000 <ice1000kotlin@foxmail.com>
2017-01-12 09:05:20 +0800

    update

---
+++ src/org/algo4j/linear/Stack.java
@@ -0,0 +1,124 @@
+package org.algo4j.linear;
+
+import org.algo4j.error.StackException;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Iterator;
+import java.util.function.Consumer;
+
+/**
+ * Created by ice1000 on 2017/1/10.
+ *
+ * @author ice1000
+ */
+@SuppressWarnings("ALL")
+public final class Stack<T> implements
+		Container<T> {
+	private final Object[] data;
+	private final int maxLen;
+	private int cursor;
+
+	@Contract(pure = true)
+	public Stack() {
+		this(1000);
+	}
+
+	public Stack(int maxLen) {
+		this.maxLen = maxLen;
+		data = new Object[maxLen];
+		cursor = 0;
+	}
+
+	private Stack(@NotNull T[] data, int maxLen, int cursor) {
+		this.data = data;
+		this.maxLen = maxLen;
+		this.cursor = cursor;
+//		java.util.Stack
+	}
+
+	@Override
+	@Contract(pure = true)
+	public int size() {
+		return cursor;
+	}
+
+	@Override
+	@Contract(pure = true)
+	public boolean empty() {
+		return size() <= 0;
+	}
+
+	@Override
+	@Nullable
+	@Contract(pure = true)
+	public T front() {
+		return empty() ? null : (T) data[cursor - 1];
+	}
+
+	@Override
+	@Nullable
+	public synchronized T pop() {
+		return empty() ? null : (T) data[--cursor];
+	}
+
+	/**
+	 * push an object
+	 *
+	 * @param obj object
+	 * @throws StackException if stack overflow
+	 */
+	@Override
+	public synchronized void push(@NotNull T obj) {
+		if (cursor > maxLen) {
+			throw StackException.overflow();
+		} else {
+			data[cursor++] = obj;
+		}
+	}
+
+	@Override
+	@NotNull
+	@Contract(" -> !null")
+	public Stack<T> clone() {
+		return new Stack<>((T[]) data.clone(), maxLen, cursor);
+	}
+
+	@Override
+	public void forEach(@NotNull Consumer<? super T> action) {
+		for (int i = 0; i < cursor; ++i) {
+			action.accept((T) data[i % maxLen]);
+		}
+	}
+
+	@Override
+	@NotNull
+	@Contract(value = " -> !null", pure = true)
+	public Iterator<T> iterator() {
+		return new StackItr(this);
+	}
+
+	public class StackItr<E> implements Iterator<E> {
+		private int cursor;
+		private Stack<E> context;
+
+		@Contract(pure = true)
+		public StackItr(@NotNull Stack<E> queue) {
+			cursor = 0;
+			context = queue;
+		}
+
+		@Override
+		@Contract(pure = true)
+		public boolean hasNext() {
+			return cursor < context.size();
+		}
+
+		@Nullable
+		@Override
+		public synchronized E next() {
+			return hasNext() ? (E) context.data[cursor++] : null;
+		}
+	}
+}
