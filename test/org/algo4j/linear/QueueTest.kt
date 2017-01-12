3347faae21f93e52b7f9b06538a95ef1794d070d
ice1000 <ice1000kotlin@foxmail.com>
2017-01-12 15:20:50 +0800

    add exceptions

---
+++ test/org/algo4j/linear/QueueTest.kt
@@ -0,0 +1,37 @@
+package org.algo4j.linear
+
+import org.jetbrains.annotations.TestOnly
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.BeforeClass
+import org.junit.Test
+
+/**
+ * Created by ice1000 on 2017/1/11.
+
+ * @author ice1000
+ */
+class QueueTest {
+	@TestOnly
+	@Test(timeout = 100)
+	fun test() {
+		val queue = Queue<Int>()
+		queue.push(233)
+		queue.push(666)
+		queue.push(123)
+		assertEquals(233, queue.front())
+		assertEquals(233, queue.pop())
+		assertEquals(666, queue.pop())
+		assertEquals(123, queue.pop())
+		assertTrue(queue.empty())
+	}
+
+	companion object Initializer {
+		@TestOnly
+		@JvmStatic
+		@BeforeClass
+		fun init() {
+			System.loadLibrary("jni")
+		}
+	}
+}
---
+++ test/org/algo4j/linear/StackTest.kt
@@ -0,0 +1,37 @@
+package org.algo4j.linear
+
+import org.jetbrains.annotations.TestOnly
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.BeforeClass
+import org.junit.Test
+
+/**
+ * Created by ice1000 on 2017/1/11.
+
+ * @author ice1000
+ */
+class StackTest {
+	@TestOnly
+	@Test(timeout = 100)
+	fun test() {
+		val stack = Stack<Int>()
+		stack.push(233)
+		stack.push(666)
+		stack.push(123)
+		assertEquals(123, stack.front())
+		assertEquals(123, stack.pop())
+		assertEquals(666, stack.pop())
+		assertEquals(233, stack.pop())
+		assertTrue(stack.empty())
+	}
+
+	companion object Initializer {
+		@TestOnly
+		@JvmStatic
+		@BeforeClass
+		fun init() {
+			System.loadLibrary("jni")
+		}
+	}
+}
