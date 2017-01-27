8faae155b3188a084bcbb3da91b3f1098d181b92
ice1000 <ice1000kotlin@foxmail.com>
2017-01-28 01:07:40 +0800

    add trie = =

---
+++ jni/global/trie.cpp
@@ -0,0 +1,5 @@
+//
+// Created by asus1 on 2017/1/28.
+//
+
+#include "trie.h"
---
+++ jni/global/trie.h
@@ -0,0 +1,8 @@
+//
+// Created by ice1000 on 2017/1/28.
+//
+
+#ifndef __ALGO4J_TRIE_H__
+#define __ALGO4J_TRIE_H__
+
+#endif //__ALGO4J_TRIE_H__
