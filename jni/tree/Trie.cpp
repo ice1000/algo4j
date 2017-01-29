f2be7cce976b12563c2ba391cb608a231c0ba53b
ice1000 <ice1000kotlin@foxmail.com>
2017-01-29 15:48:22 +0800

    add Trie.cpp Trie.h

---
+++ jni/tree/Trie.cpp
@@ -0,0 +1,78 @@
+
+#include "../global/trie.h"
+#include "../global/basics.hpp"
+#include "Trie.h"
+
+#include <stdio.h>
+
+using algo4j_trie::Trie;
+using algo4j_trie::Node;
+
+JNIEXPORT auto JNICALL Java_org_algo4j_tree_Trie_createTrie(
+		JNIEnv *,
+		jclass) -> jlong {
+	return (jlong) new Trie();
+}
+
+JNIEXPORT auto JNICALL Java_org_algo4j_tree_Trie_deleteTrie(
+		JNIEnv *,
+		jclass,
+    jlong ptr) -> void {
+	delete (Trie *) ptr;
+}
+
+JNIEXPORT auto JNICALL Java_org_algo4j_tree_Trie_insert(
+		JNIEnv *env,
+		jobject,
+		jlong ptr,
+		jbyteArray _word) -> void {
+	__JNI__FUNCTION__INIT__
+	auto trie = (Trie *) ptr;
+	__get(Byte, word);
+	trie->insert(word, __len(word));
+	__abort(Byte, word);
+	__JNI__FUNCTION__CLEAN__
+}
+
+JNIEXPORT auto JNICALL Java_org_algo4j_tree_Trie_remove(
+		JNIEnv *env,
+		jobject,
+		jlong ptr,
+		jbyteArray _word) -> void {
+	__JNI__FUNCTION__INIT__
+	auto trie = (Trie *) ptr;
+	__get(Byte, word);
+	trie->remove(word, __len(word));
+	__abort(Byte, word);
+	__JNI__FUNCTION__CLEAN__
+}
+
+JNIEXPORT auto JNICALL Java_org_algo4j_tree_Trie_contains(
+		JNIEnv *env,
+		jobject,
+		jlong ptr,
+		jbyteArray _word) -> jboolean {
+	__JNI__FUNCTION__INIT__
+	auto trie = (Trie *) ptr;
+	__get(Byte, word);
+	auto ret = trie->exist(word, __len(word));
+	__abort(Byte, word);
+	__JNI__FUNCTION__CLEAN__
+	return ret ? JNI_TRUE : JNI_FALSE;
+}
+
+JNIEXPORT auto JNICALL Java_org_algo4j_tree_Trie_containsPrefix(
+		JNIEnv *env,
+		jobject,
+		jlong ptr,
+		jbyteArray _word) -> jboolean {
+	__JNI__FUNCTION__INIT__
+	auto trie = (Trie *) ptr;
+	__get(Byte, word);
+	auto ret = trie->existPrefix(word, __len(word));
+	__abort(Byte, word);
+	__JNI__FUNCTION__CLEAN__
+	return ret ? JNI_TRUE : JNI_FALSE;
+}
+
+
