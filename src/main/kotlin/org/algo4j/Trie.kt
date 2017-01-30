2232d1439d0d25b10eee9580a7297e863bc900cd
ice1000 <ice1000kotlin@foxmail.com>
2017-01-30 16:56:33 +0800

    add extension

---
+++ src/main/kotlin/org/algo4j/Trie.kt
@@ -0,0 +1,38 @@
+@file:JvmName("Extension")
+@file:JvmMultifileClass
+
+/**
+ * Created by ice1000 on 2017/1/30.
+ *
+ * @author ice1000
+ */
+package org.algo4j
+
+import org.algo4j.tree.Trie
+import org.jetbrains.annotations.Contract
+
+@Contract(pure = false)
+operator fun Trie.plusAssign(word: String) =
+		insert(word)
+
+@Contract(pure = false)
+operator fun Trie.plusAssign(word: ByteArray) =
+		insert(word)
+
+@Contract(pure = false)
+operator fun Trie.minusAssign(word: String) =
+		remove(word)
+
+@Contract(pure = false)
+operator fun Trie.minusAssign(word: ByteArray) =
+		remove(word)
+
+@Contract(pure = true)
+operator fun Trie.contains(word: String) =
+		contains(word)
+
+@Contract(pure = true)
+operator fun Trie.contains(word: ByteArray) =
+		contains(word)
+
+
