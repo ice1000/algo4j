f57f439040127dc8ae6074df18c8e2d60c4c05d8
ice1000 <ice1000kotlin@foxmail.com>
2017-04-16 01:36:43 +0800

    turn 'loadLibrary' into 'load' and give absolute path

--- src/main/java/org/algo4j/util/Loader.java
+++ src/main/java/org/algo4j/util/Loader.java
@@ -5,9 +5,6 @@ import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.io.OutputStream;
 
 /**
  * To load the JNI library
@@ -17,39 +14,15 @@ import java.io.OutputStream;
  */
 @SuppressWarnings("WeakerAccess")
 public final class Loader {
-	private static final String LIB_BIN;
 	public final static String JNI_LIB_NAME;
+	private static boolean loaded = false;
 
 	/*
 	 * maybe it's already loaded, so there should be a check
 	 */
 	static {
-		LIB_BIN = "/lib-bin/";
 		JNI_LIB_NAME = "jni";
-		try {
-			loadJni();
-		} catch (UnsatisfiedLinkError e) {
-			loadLib(JNI_LIB_NAME);
-		}
-	}
-
-	/**
-	 * Puts library to temp dir and loads to memory
-	 * for windows only
-	 */
-	private static void loadLib(String ___) {
-		String __ = ___ + ".dll";
-		try {
-			InputStream in = Loader.class.getResourceAsStream(LIB_BIN + __);
-			File fileOut = new File(__);
-			OutputStream out = new FileOutputStream(fileOut);
-			byte[] ____ = new byte[0xFFFF];
-			while (-1 != in.read(____)) out.write(____);
-			in.close();
-			out.close();
-			System.load(fileOut.toString());
-		} catch (Exception ignored) {
-		}
+		loadJni();
 	}
 
 	@NotNull
@@ -59,13 +32,15 @@ public final class Loader {
 	}
 
 	public static void loadJni() {
-		System.loadLibrary(JNI_LIB_NAME);
-		String ___ = System.getProperty("os.name");
-		if (___.contains("Linux"))
-			System.load(getJniPath("lib" + JNI_LIB_NAME + ".so"));
-		else if (___.contains("Windows"))
-			System.load(getJniPath(JNI_LIB_NAME + ".dll"));
-		else if (___.contains("OSX"))
-			System.load(getJniPath(JNI_LIB_NAME + ".dylib"));
+		if (!loaded) {
+			String ___ = System.getProperty("os.name");
+			if (___.contains("Linux"))
+				System.load(getJniPath("lib" + JNI_LIB_NAME + ".so"));
+			else if (___.contains("Windows"))
+				System.load(getJniPath(JNI_LIB_NAME + ".dll"));
+			else if (___.contains("OSX"))
+				System.load(getJniPath("lib" + JNI_LIB_NAME + ".dylib"));
+			loaded = true;
+		}
 	}
 }
